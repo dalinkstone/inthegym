295ce8aeefa14a530e8b5819374ee8777e5423fe
dalinkstone <dalinkstone@gmail.com>
2025-12-18 16:34:56 -0700

    this adds the template for validanagram in golang

---
+++ easy/validanagram/go/validana.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func validate(first, second string) bool {
+
+	return false
+}
+
+func main() {
+	fmt.Println("This should return false, testing jar and jam")
+	result1 := validate("jar", "jam")
+}
