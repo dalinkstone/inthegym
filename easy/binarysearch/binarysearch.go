d27689d1770fee1e8302a97fef213bbe5121e443
dalinkstone <dalinkstone@gmail.com>
2025-12-17 18:11:20 -0700

    another commit

---
+++ easy/binarysearch/binarysearch.go
@@ -0,0 +1,22 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func binarysearch(nums array, target int) bool {
+	int l, r = 0, len(nums)
+
+	for l < r {
+		mid = (l + r) / len(nums)
+
+		if nums[mid] == target {
+			return true
+		}
+
+	}
+}
+
+func main() int {
+
+}
