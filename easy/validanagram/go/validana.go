61bfe901824584f84a9e641f2d7526f2d4825951
dalinkstone <dalinkstone@gmail.com>
2025-12-18 20:55:44 -0700

    this completes validanagram in golang

--- easy/validanagram/go/validana.go
+++ easy/validanagram/go/validana.go
@@ -10,13 +10,28 @@ func validate(first, second string) bool {
 		return false
 	}
 
-	countFirst := make(map[string]int)
-	countSecond := make(map[string]int)
+	count := [26]int{}
 
-	return false
+	for i := range len(first) {
+		count[first[i]-'a']++
+		count[second[i]-'a']--
+	}
+
+	for _, val := range count {
+		if val != 0 {
+			return false
+		}
+	}
+
+	return true
 }
 
 func main() {
 	fmt.Println("This should return false, testing jar and jam")
 	result1 := validate("jar", "jam")
+	fmt.Printf("%v/n", result1)
+
+	fmt.Println("This should return true, testing racecar and carrace")
+	result2 := validate("racecar", "carrace")
+	fmt.Printf("%v\n", result2)
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/dalinkstone/inthegym
 
-<<<<<<< HEAD
 go 1.25.4
-=======
-go 1.24.5
->>>>>>> dc3c865 (this is something)
