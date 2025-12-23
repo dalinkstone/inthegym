41cd1abd95daa7f3c02f5ea0a11c9a3ee869c5b4
dalinkstone <dalinkstone@gmail.com>
2025-12-22 23:17:36 -0700

    this adds fibonacci to recursive in golang

--- easy/recursion/go/recurs.go
+++ easy/recursion/go/recurs.go
@@ -13,8 +13,24 @@ func recurs(num int) int {
 
 }
 
+func fibo(num int) int {
+	if num == 0 {
+		return 0
+	}
+
+	if num == 1 {
+		return 1
+	}
+
+	return fibo(num - 1) + fibo(num - 2)
+}
+
 func main() {
 	fmt.Println("This should return 385, we are passing the number 10")
 	result := recurs(10)
 	fmt.Printf("%v\n", result)
+
+	fmt.Println("This should return the fibonacci of 10 intervals")
+	result2 := fibo(10)
+	fmt.Printf("%v\n", result2)
 }
