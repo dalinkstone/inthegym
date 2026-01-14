7732ff03b05ba930fe5dc6958f39fd19129b2dba
dalinkstone <dalinkstone@gmail.com>
2026-01-14 16:22:45 -0700

    this completes part 1 of week 2 tasks with hash collisions

--- algohall/hashing/hash.py
+++ algohall/hashing/hash.py
@@ -1,21 +1,68 @@
-import random
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
 
 
-class HashSolver:
-    def __init__(self):
-        self.id = 0
+class Table:
+    def __init__(self, size=7):
+        self.size = size
+        self.elements = [None] * self.size
 
-    def hasher(self, key: int) -> int:
+    def hash(self, key: int) -> int:
         return ((8 * key) + 2) % 7
+
+    def insert(self, key, value):
+        index = self.hash(int(key))
+
+        if self.elements[index] is None:
+            self.elements[index] = Node(key, value)
+
+        current = self.elements[index]
+        prev = None
+
+        while current:
+            if current.key == key:
+                current.value = value
+                return
+
+            prev = current
+            current = current.next
+
+        new_node = Node(key, value)
+        prev.next = new_node
+
+    def print_table(self) -> None:
+        printable = []
+        for i in range(self.size):
+            current = self.elements[i]
+
+            table = []
+
+            while current:
+                table.append(str(current.key))
+                current = current.next
+
+            if not table:
+                printable.append("Null")
+            else:
+                printable.append(table)
+
+        print(printable)
 
 
 def main():
-    nums: list[int] = [3, 18, 42, 31, 40, 56, 71]
+    table = Table()
+    table.insert("3", 3)
+    table.insert("18", 18)
+    table.insert("42", 42)
+    table.insert("31", 31)
+    table.insert("40", 40)
+    table.insert("56", 56)
+    table.insert("71", 71)
 
-    new_solver = HashSolver()
-
-    for num in nums:
-        print(new_solver.hasher(num))
+    table.print_table()
 
 
 if __name__ == "__main__":
